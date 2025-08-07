0225a3658c1370054fba1625cf4f8323dc76bf96
talesofcarter <kelvinjuma99@gmail.com>
2025-08-07 13:29:31 +0300

    First commit

---
+++ week2_assignment.py
@@ -0,0 +1,24 @@
+# create an empty list
+my_list = []
+
+# append items
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# insert items
+my_list.insert(1, 15)
+
+# extend items
+my_list.extend([50, 60, 70])
+
+# remove last element
+my_list.pop()
+
+# sort list
+my_list.sort()
+
+# find index
+index_value = my_list.index(30)
+print(index_value)

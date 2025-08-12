5df9a231ce265ad72aa3f8c781285d8b18126cbe
talesofcarter <kelvinjuma99@gmail.com>
2025-08-12 15:23:36 +0300

    finalize

--- week3_assignment.py
+++ week3_assignment.py
@@ -6,9 +6,16 @@ def calculate_discount(price, discount_percent):
     else:
         return price
 
+
 # prompt the user to enter the initial price and percentage discount
 marked_price = float(input("Enter the initial price: "))
 discount_percent = float(input("Enter the percentage discount: "))
 
 final_price = calculate_discount(marked_price, discount_percent)
-print(f"The final price is {final_price}")
+
+if discount_percent >= 20:
+    print(
+        f"You are eligible for a {discount_percent}% discount, the final price is {final_price}"
+    )
+else:
+    print(f"You have not qualified for any discount today. Kindly pay {marked_price}")
